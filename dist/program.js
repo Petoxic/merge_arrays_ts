9db3e571f275a43a957665c955b11fdb739d508e
Pattanan Piyachotinan <70986870+Petoxic@users.noreply.github.com>
2022-12-21 22:16:34 +0700

    add merge function

--- dist/program.js
+++ dist/program.js
@@ -1,1 +1,22 @@
 "use strict";
+function merge(collection_1, collection_2) {
+    const len_1 = collection_1.length;
+    const len_2 = collection_2.length;
+    let answer = [];
+    let index_1 = 0;
+    let index_2 = 0;
+    while (index_1 < len_1 || index_2 < len_2) {
+        if (index_1 >= len_1) {
+            answer.push(collection_2[index_2++]);
+        }
+        else if (index_2 >= len_2) {
+            answer.push(collection_1[index_1++]);
+        }
+        else {
+            collection_1[index_1] < collection_2[index_2]
+                ? answer.push(collection_1[index_1++])
+                : answer.push(collection_2[index_2++]);
+        }
+    }
+    return answer;
+}
---
+++ src/program.ts
@@ -0,0 +1,29 @@
+function merge(collection_1: number[], collection_2: number[]): number[] {
+    // length of two arrays
+    const len_1: number = collection_1.length;
+    const len_2: number = collection_2.length;
+
+    // return variable
+    let answer: number[] = [];
+
+    // index for each array for checking
+    let index_1: number = 0;
+    let index_2: number = 0;
+
+    // loop until every number are in answer
+    while (index_1 < len_1 || index_2 < len_2) {
+        if (index_1 >= len_1) {
+            //if all number in collection_1 are in answer
+            answer.push(collection_2[index_2++]);
+        } else if (index_2 >= len_2) {
+            //if all number in collection_2 are in answer
+            answer.push(collection_1[index_1++]);
+        } else {
+            // compare two numbers and insert the lesser one
+            collection_1[index_1] < collection_2[index_2] 
+            ? answer.push(collection_1[index_1++]) 
+            : answer.push(collection_2[index_2++]);
+        }
+    }
+    return answer;
+}
